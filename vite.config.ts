edf0e22582bcf4ce54d6e9c7c0646a9974af4168
cdryampi <cdryampi@gmail.com>
2025-05-30 19:19:05 +0200

    feat: Update deployment configuration and add Nixpacks support

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,18 @@ export default defineConfig({
     include: ['@ckeditor/ckeditor5-build-classic']
   },
   server: {
+    host: '0.0.0.0',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://jobless-journal.yampi.eu',
+        changeOrigin: true
+      }
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
       '/api': {
         target: 'https://jobless-journal.yampi.eu',
